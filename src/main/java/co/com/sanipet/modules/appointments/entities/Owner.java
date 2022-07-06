a79f22febe2293e84146e62ac5901e408fa5d8b7
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-06 15:08:55 -0500

    oops, added the getters and toString to Owner.java

--- src/main/java/co/com/sanipet/modules/appointments/entities/Owner.java
+++ src/main/java/co/com/sanipet/modules/appointments/entities/Owner.java
@@ -3,11 +3,20 @@ package co.com.sanipet.modules.appointments.entities;
 public class Owner extends Person {
     private final Integer age;
 
-    public Owner(String DNI, String name, String surname, String cellphone,  Integer age) {
+    public Owner(String DNI, String name, String surname, String cellphone, Integer age) {
         this.age = age;
         this.DNI = DNI;
         this.cellphone = cellphone;
         this.name = name;
         this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" + "age=" + age + ", DNI='" + DNI + '\'' + ", name='" + name + '\'' + ", surname='" + surname + '\'' + ", cellphone='" + cellphone + '\'' + '}';
     }
 }
