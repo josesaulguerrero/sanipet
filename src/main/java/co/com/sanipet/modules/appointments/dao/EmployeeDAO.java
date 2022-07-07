10d4c1ec2b80af1ce8c7142f50add0eaf13b95ca
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-07 09:56:59 -0500

    refactored the employee dao

--- src/main/java/co/com/sanipet/modules/appointments/dao/EmployeeDAO.java
+++ src/main/java/co/com/sanipet/modules/appointments/dao/EmployeeDAO.java
@@ -2,30 +2,45 @@ package co.com.sanipet.modules.appointments.dao;
 
 import co.com.sanipet.modules.appointments.entities.Employee;
 import co.com.sanipet.modules.appointments.entities.Roles;
+import co.com.sanipet.modules.appointments.entities.Schedule;
 import co.com.sanipet.modules.appointments.entities.WorkingDays;
 
-import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class EmployeeDAO {
+
+    HashMap<WorkingDays, String> firstHalfSchedule = new HashMap<>();
+    HashMap<WorkingDays, String> secondHalfSchedule = new HashMap<>();
+    HashMap<WorkingDays, String> wholeWeekSchedule = new HashMap<>();
+
     List<Employee> employees = List.of(
             new Employee("12312432", "John", "Newman", "1239872348",
-                    Roles.DOCTOR, new WorkingDays[]{WorkingDays.MONDAY, WorkingDays.TUESDAY, WorkingDays.WEDNESDAY}),
+                    Roles.DOCTOR, new Schedule(firstHalfSchedule)),
             new Employee("79872493", "Diana", "Wilson", "9873298472",
-                    Roles.DOCTOR, new WorkingDays[]{WorkingDays.MONDAY, WorkingDays.TUESDAY, WorkingDays.WEDNESDAY}),
+                    Roles.DOCTOR, new Schedule(firstHalfSchedule)),
             new Employee("80927434", "Ivan", "Ivanovich", "9023749025",
-                    Roles.DOCTOR, new WorkingDays[]{WorkingDays.THURSDAY, WorkingDays.FRIDAY, WorkingDays.SATURDAY}),
+                    Roles.DOCTOR, new Schedule(secondHalfSchedule)),
             new Employee("09808908", "Elizabeth", "Mcbride", "9023749025",
-                    Roles.STYLIST, new WorkingDays[]{WorkingDays.MONDAY,
-                    WorkingDays.TUESDAY, WorkingDays.WEDNESDAY, WorkingDays.THURSDAY,
-                    WorkingDays.FRIDAY, WorkingDays.SATURDAY}),
+                    Roles.STYLIST, new Schedule(wholeWeekSchedule)),
             new Employee("09808908", "Brenda", "Cooper", "9023749025",
-                    Roles.STYLIST, new WorkingDays[]{WorkingDays.MONDAY,
-                    WorkingDays.TUESDAY, WorkingDays.WEDNESDAY, WorkingDays.THURSDAY,
-                    WorkingDays.FRIDAY, WorkingDays.SATURDAY})
+                    Roles.STYLIST, new Schedule(wholeWeekSchedule))
     );
+
+    public EmployeeDAO() {
+        String normalOperationHours = "08:00 - 19:00";
+        String specialOperationHours = "09:00 - 15:00";
+        firstHalfSchedule.put(WorkingDays.MONDAY, normalOperationHours);
+        firstHalfSchedule.put(WorkingDays.TUESDAY, normalOperationHours);
+        firstHalfSchedule.put(WorkingDays.WEDNESDAY, normalOperationHours);
+        // -----------------------
+        secondHalfSchedule.put(WorkingDays.THURSDAY, normalOperationHours);
+        secondHalfSchedule.put(WorkingDays.FRIDAY, normalOperationHours);
+        secondHalfSchedule.put(WorkingDays.SATURDAY, specialOperationHours);
+        // ------------------------
+        wholeWeekSchedule.putAll(firstHalfSchedule);
+        wholeWeekSchedule.putAll(secondHalfSchedule);
+    }
 
     public List<Employee> findAll() {
         return employees;
@@ -37,10 +52,6 @@ public class EmployeeDAO {
     }
 
     public List<Employee> findAvailable() {
-        return employees.stream().filter((employee) -> {
-            WorkingDays today =
-                    WorkingDays.valueOf(LocalDate.now().getDayOfWeek().name());
-            return Arrays.asList(employee.getSchedule().getActiveDays()).contains(today);
-        }).collect(Collectors.toList());
+        // TODO find available
     }
 }
