842dadf4928a1cdc5a0019d3b67c9fab054f69a9
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-07 15:29:34 -0500

    created methods to validate the user input

--- src/main/java/co/com/sanipet/utils/ConsoleMenu.java
+++ src/main/java/co/com/sanipet/utils/ConsoleMenu.java
@@ -1,17 +1,35 @@
 package co.com.sanipet.utils;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class ConsoleMenu {
-    public static String renderOptionsList(String ...options) {
+    public static String renderAndRead(String ...options) {
         Arrays.stream(options).forEachOrdered(System.out::println);
         return readLineFromConsole();
     }
 
-    public static String renderQuestion(String question) {
+    public static String renderAndRead(String question) {
         System.out.println(question);
         return readLineFromConsole();
+    }
+
+    public static String renderAndVerify(Predicate<String> validator, String ...options) {
+        Optional<String> option = Optional.of("");
+        while(!validator.test(option.get())) {
+            option = Optional.of(renderAndRead(options));
+        }
+        return option.get();
+    }
+
+    public static String renderAndVerify(Predicate<String> validator, String question) {
+        Optional<String> answer = Optional.of("");
+        while(!validator.test(answer.get())) {
+            answer = Optional.of(renderAndRead(question));
+        }
+        return answer.get();
     }
 
     private static String readLineFromConsole() {
