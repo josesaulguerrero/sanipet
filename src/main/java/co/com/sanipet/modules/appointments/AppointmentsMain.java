eda3997f0803ba4eebc7f08857bc0a662d40e9c8
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-07 19:40:35 -0500

    added the getAppointmentInformation method

--- src/main/java/co/com/sanipet/modules/appointments/AppointmentsMain.java
+++ src/main/java/co/com/sanipet/modules/appointments/AppointmentsMain.java
@@ -1,10 +1,10 @@
 package co.com.sanipet.modules.appointments;
 
 import co.com.sanipet.modules.appointments.dao.EmployeeDAO;
-import co.com.sanipet.modules.appointments.entities.Animals;
-import co.com.sanipet.modules.appointments.entities.Owner;
-import co.com.sanipet.modules.appointments.entities.Patient;
+import co.com.sanipet.modules.appointments.entities.*;
 import co.com.sanipet.utils.ConsoleMenu;
+import org.apache.commons.lang3.Range;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.time.LocalDate;
 import java.util.Locale;
@@ -14,16 +14,13 @@ public class AppointmentsMain {
 
     private static final EmployeeDAO employeeDAO = new EmployeeDAO();
     public static void main() {
-        Optional<Integer> selectedOption = Optional.empty();
-        while (selectedOption.isEmpty()){
-            try {
-                selectedOption = Optional.of(Integer.parseInt(ConsoleMenu.renderOptionsList("1. Register new appointment", "2. Display " +
-                        "history")));
-                pickOption(selectedOption.get());
-            } catch (Exception e) {
-                System.out.println("The supplied information is incorrect.");
-            }
-        }
+        Optional<Integer> selectedOption = Optional.of(
+                Integer.valueOf(ConsoleMenu.renderAndVerify(
+                        (option) -> NumberUtils.isParsable(option) && Range.between(1, 2).contains(Integer.parseInt(option)),
+                        "1. Register new appointment", "2. Display history"
+                ))
+        );
+        pickOption(selectedOption.get());
     }
 
     private static void pickOption(int option) {
@@ -38,40 +35,64 @@ public class AppointmentsMain {
     }
 
     private static void registerNewAppointment() {
-        // TODO get owner information from user
-        // TODO get patient information from user
-        // TODO get appointment information form user
+        Owner owner = getOwnerInformation();
+        Patient patient = getPatientInformation(owner);
+        Appointment appointment =  getAppointmentInformation(patient);
     }
 
     private static Owner getOwnerInformation() {
         System.out.println("Before we start, we need some information from the owner!");
-        String DNI  = ConsoleMenu.renderQuestion("Enter your DNI number:");
-        String name = ConsoleMenu.renderQuestion("What is your name?");
-        String surname = ConsoleMenu.renderQuestion("What is your surname?");
-        String cellphone = ConsoleMenu.renderQuestion("What is your cellphone contact number?");
-        Optional<Integer> age = Optional.empty();
-        while (age.isEmpty()){
-            try {
-                age = Optional.of(Integer.parseInt(ConsoleMenu.renderOptionsList("")));
-            } catch (Exception e) {
-                System.out.println("You must be over 18 years old. Please enter a valid age.");
-            }
-        }
+        String DNI  = ConsoleMenu.renderAndRead("Enter your DNI number:");
+        String name = ConsoleMenu.renderAndRead("What is your name?");
+        String surname = ConsoleMenu.renderAndRead("What is your surname?");
+        String cellphone = ConsoleMenu.renderAndRead("What is your cellphone contact number?");
+        Optional<Integer> age = Optional.of(Integer.parseInt(ConsoleMenu.renderAndVerify(
+                (answer) -> NumberUtils.isParsable(answer) && Integer.parseInt(answer) >= 18,
+                "How old are you? You must be over 18"
+        )));
         return new Owner(DNI, name, surname, cellphone, age.get());
     }
 
-    private static Patient patientInfo(Owner owner){
-        Animals patientSpecies = Animals.valueOf(ConsoleMenu.renderQuestion("What is the species of the patient? " +
+    private static Patient getPatientInformation(Owner owner){
+        Animals patientSpecies = Animals.valueOf(ConsoleMenu.renderAndRead("What is the species of the patient? " +
                 "(DOG/CAT)"));
-        String patientName = ConsoleMenu.renderQuestion("What is the name of the patient?");
-        String patientBreed = ConsoleMenu.renderQuestion("What is the breed of the patient?");
+        String patientName = ConsoleMenu.renderAndRead("What is the name of the patient?");
+        String patientBreed = ConsoleMenu.renderAndRead("What is the breed of the patient?");
         Boolean patientVaccinated =
-                ConsoleMenu.renderQuestion("Is the patient vaccinated? (YES/NO)").toUpperCase(Locale.ROOT).equals("YES");
-        LocalDate patientLastDeworming = LocalDate.parse(ConsoleMenu.renderQuestion("When the patient was dewormed? " +
+                ConsoleMenu.renderAndRead("Is the patient vaccinated? (YES/NO)").toUpperCase(Locale.ROOT).equals("YES");
+        LocalDate patientLastDeworming = LocalDate.parse(ConsoleMenu.renderAndRead("When the patient was dewormed? " +
                 "(YYYY-MM-DD)"));
-
         return new Patient(patientSpecies, patientName, patientBreed, patientVaccinated, patientLastDeworming, owner);
     }
+
+    private static Appointment getAppointmentInformation(Patient patient){
+        System.out.println("What kind of appointment do you need?");
+        int selectedOption = Integer.parseInt(ConsoleMenu.renderAndVerify(
+                        (option) -> NumberUtils.isParsable(option) && Range.between(1, 3).contains(Integer.parseInt(option)),
+                        "1. Medical", "2. Surgical", "3. Aesthetical"
+        ));
+        AppointmentTypes type = getAppointmentType(selectedOption);
+        WorkingDays day = WorkingDays.valueOf(ConsoleMenu.renderAndRead("Which day of the week do you prefer the " +
+                "appointment in?").toUpperCase(Locale.ROOT));
+        System.out.println(employeeDAO.findAvailable(
+                type == AppointmentTypes.MEDICAL || type == AppointmentTypes.SURGERY ? Roles.DOCTOR : Roles.STYLIST, day
+        ));
+        return null;
+    }
+
+    private static AppointmentTypes getAppointmentType(int option) {
+        switch (option){
+            case 1:
+            default:
+                return AppointmentTypes.MEDICAL;
+            case 2:
+                return AppointmentTypes.SURGERY;
+            case 3:
+                return AppointmentTypes.AESTHETIC;
+        }
+    }
+
+
     private static void displayHistory() {
 
     }
