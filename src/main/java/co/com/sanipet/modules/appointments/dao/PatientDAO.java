852602efd82e3898e7ae1fdea9a6a6cba1dca905
Andres <andresamayachaves@gmail.com>
2022-07-08 12:36:30 -0500

    Class PatientDAO was created with its structure.

---
+++ src/main/java/co/com/sanipet/modules/appointments/dao/PatientDAO.java
@@ -0,0 +1,35 @@
+package co.com.sanipet.modules.appointments.dao;
+
+import co.com.sanipet.modules.appointments.entities.*;
+
+public class PatientDAO {
+    //Attributes
+
+    /*
+    * Method to log in the user
+    * @param clinicalHistoryId
+    * */
+    //logIn(String clinicalHistoryId) {}
+
+    /*
+     * Method to sign up the user
+     * */
+    //signUp() {}
+
+    /*
+     * Method to seek a patient by Clinical History ID
+     * @param clinicalHistoryId
+     * */
+    //seekByClinicalHistoryId(String clinicalHistoryId) {}
+
+    /*
+     * Method to create a patient
+     * */
+    //create() {}
+
+    /*
+     * Method to save the patient in DAO.
+     * @param DNI
+     * */
+    //save(Patient patient) {}
+}
