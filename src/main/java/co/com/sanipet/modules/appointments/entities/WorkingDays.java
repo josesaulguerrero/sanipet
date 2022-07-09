a757888a43ef0ff6e33adb1ae94d8c4183e1c3db
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-09 09:50:54 -0500

    added a constructor to the WorkingDays enum

--- src/main/java/co/com/sanipet/modules/appointments/entities/WorkingDays.java
+++ src/main/java/co/com/sanipet/modules/appointments/entities/WorkingDays.java
@@ -1,10 +1,20 @@
 package co.com.sanipet.modules.appointments.entities;
 
 public enum WorkingDays {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY;
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6);
+
+    private final Integer weekday;
+
+    WorkingDays(int weekday) {
+        this.weekday = weekday;
+    }
+
+    public Integer getWeekday() {
+        return weekday;
+    }
 }
